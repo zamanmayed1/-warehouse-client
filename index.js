0cbbb31c23e34802a3b23edeb68c99e59a98ba6b
zamanmayed1 <zamanmayed02@gmail.com>
2022-05-02 01:33:46 +0600

    Env File Added

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config()
 const bodyParser = require('body-parser')
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express()
 
@@ -13,7 +14,7 @@ app.use(bodyParser.json())
 
 
 
-const uri = "mongodb+srv://mayedstockroom:iePuKsxvX5t5tBfT@cluster0.2jmak.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.2jmak.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
